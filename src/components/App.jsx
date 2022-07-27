0f777080ad6fc8679c76e3c7613e37c8a0b56278
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-07-28 00:15:36 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,55 @@
-import { Component } from 'react';
+
 import { Filter } from './Filter/Filter';
 import { Form } from '../components/Form/Form';
 import { Section } from './Section/Section';
 
 import { ContactsList } from './ContactsList/ContactsList';
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+import { useState } from 'react';
+import { useEffect } from 'react';
+export const App = () => {
 
-  componentDidMount(){
-    const contacts = (localStorage.getItem('contacts'))
-     if(contacts){
-       this.setState({contacts: JSON.parse(contacts)})
-     }
-   }
-   componentDidUpdate(prevProp, prevState){
-     if (prevState.contacts!==this.state.contacts) {
-       localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-     }
-     
-   }
- 
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
 
-  addContactsData = contact => {
-    const{contacts}=this.state
-    if(contacts.some(el=>el.name.toLowerCase()===contact.name.toLowerCase())){
-      alert(`${contact.name} is already in contacts`)
-      return
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContactsData = contact => {
+    if (
+      contacts.some(el => el.name.toLowerCase() === contact.name.toLowerCase())
+    ) {
+      alert(`${contact.name} is already in contacts`);
+      return;
     }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
+    setContacts(prev => [...prev, contact]);
   };
 
-  onChangeFilter = ({ target: { value } }) => {
-    
-    this.setState({
-      filter: value,
-    });
+  const onChangeFilter = ({ target: { value } }) => {
+    setFilter(value);
   };
 
-  onDelete=(id)=>{
-    this.setState(prev=>({
-      contacts: prev.contacts.filter(el=>el.id!==id)
-    }))
-    
-  }
+  const onDelete = id => {
+    setContacts(prev => prev.filter(el => el.id !== id));
+  };
 
-
-
-  filterContacts=()=>{
-    
-   const{filter, contacts}= this.state
-   return contacts.filter((el)=>el.name.toLowerCase().includes(filter.toLowerCase()))
-  }
-
-  render() {
-    
-    return (
-      <>
-        <Section title={'PhoneBook'}>
-          <Form addContactsData={this.addContactsData} />
-        </Section>
-        <Section title={'Contacts'}>
-          <Filter  onChange={this.onChangeFilter} />
-          <ContactsList contacts={this.filterContacts()} onDelete={this.onDelete} />
-        </Section>
-      </>
+  const filterContacts = () => {
+    return contacts.filter(el =>
+      el.name.toLowerCase().includes(filter.toLowerCase())
     );
-  }
-}
+  };
+
+  return (
+    <>
+      <Section title={'PhoneBook'}>
+        <Form addContactsData={addContactsData} />
+      </Section>
+      <Section title={'Contacts'}>
+        <Filter onChange={onChangeFilter} />
+        <ContactsList contacts={filterContacts()} onDelete={onDelete} />
+      </Section>
+    </>
+  );
+};
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,69 +1,64 @@
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
-import PropTypes from 'prop-types'
-import styles from '../Form/Form.module.css'
-export class Form extends Component {
-  static propTypes={
-    addContactsData: PropTypes.func.isRequired,
-  }
-  state = {
-    name: '',
-    number: '',
-  }
-
-  onChange = ({ target: { name, value } }) => {
-    
-    this.setState({
-      [name]: value,
-    });
+import PropTypes from 'prop-types';
+import styles from '../Form/Form.module.css';
+import { useState } from 'react';
+export const Form = ({ addContactsData }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const onChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  onSubmit=evt=>{
+  const onSubmit = evt => {
     evt.preventDefault();
     let id = nanoid();
-    
-    const contact = { ...this.state, id };
-    this.props.addContactsData(contact);
 
-    this.setState({
-      name: '',
-      number: '',
-    })
-    
-  }
+    const contact = { name, number, id };
+    addContactsData(contact);
 
-  render() {
-    const{name, number}=this.state
-    return (
-      <form className={styles.form} onSubmit={this.onSubmit}>
-        <label className={styles.label}>
-          Name
-          <input
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form className={styles.form} onSubmit={onSubmit}>
+      <label className={styles.label}>
+        Name
+        <input
           value={name}
-            className={styles.input}
-            type="text"
-            name="name"
-            required
-            onChange={this.onChange}
-          />
-        </label>
-        <label
-        className={styles.label}>
-        
-          Number
-          <input
+          className={styles.input}
+          type="text"
+          name="name"
+          required
+          onChange={onChange}
+        />
+      </label>
+      <label className={styles.label}>
+        Number
+        <input
           className={styles.input_last}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            type="tel"
-            name="number"
-            value={number}
-            required
-            onChange={this.onChange}
-          />
-        </label>
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          type="tel"
+          name="number"
+          value={number}
+          required
+          onChange={onChange}
+        />
+      </label>
 
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  }
-}
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
+Form.propTypes = {
+  addContactsData: PropTypes.func.isRequired,
+};
